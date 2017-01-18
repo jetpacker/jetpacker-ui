3f63ab7bf8066ee3b2fc646f1b28308466bdb1c5
forseti <slickwolf@gmail.com>
2017-01-19 00:03:45 +0800

    Add Navigation.vue

---
+++ src/components/Navigation.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="panel-group" id="accordion">
+    <div class="panel panel-default">
+      <div class="panel-heading">
+        <h4 class="panel-title">
+          Machines
+        </h4>
+      </div>
+      <div id="collapseOne" class="panel-collapse collapse in">
+        <div class="panel-body">
+          <p>Ubuntu</p>
+        </div>
+      </div>
+    </div>
+    <div class="panel panel-default">
+      <div class="panel-heading">
+        <h4 class="panel-title">
+          SDKs
+        </h4>
+      </div>
+      <div id="sdks" class="panel-collapse collapse in">
+        <div class="panel-body">
+          <p>OpenJDK</p>
+          <p>Node.js</p>
+          <p>Guard</p>
+        </div>
+      </div>
+    </div>
+    <div class="panel panel-default">
+      <div class="panel-heading">
+        <h4 class="panel-title">
+          Database Servers
+        </h4>
+      </div>
+      <div id="databaseServers" class="panel-collapse collapse in">
+        <div class="panel-body">
+
+        </div>
+      </div>
+    </div>
+    <div class="panel panel-default">
+      <div class="panel-heading">
+        <h4 class="panel-title">
+          Message Brokers
+        </h4>
+      </div>
+      <div id="messageBrokers" class="panel-collapse collapse in">
+        <div class="panel-body">
+        </div>
+      </div>
+    </div>
+    <div class="panel panel-default">
+      <div class="panel-heading">
+        <h4 class="panel-title">
+          Search Engines
+        </h4>
+      </div>
+      <div id="searchEngines" class="panel-collapse collapse in">
+        <div class="panel-body">
+        </div>
+      </div>
+    </div>
+  </div>
+</template>

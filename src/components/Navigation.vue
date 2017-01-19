803c8512099f60b97ee99c284041c823581ed584
ruach <slickwolf@gmail.com>
2017-01-19 14:10:42 +0800

    Added initial Header with Navigation Bar

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,62 +1,71 @@
 <template>
-  <div class="panel-group" id="accordion">
-    <div class="panel panel-default">
-      <div class="panel-heading">
-        <h4 class="panel-title">
-          Machines
-        </h4>
-      </div>
-      <div id="collapseOne" class="panel-collapse collapse in">
-        <div class="panel-body">
-          <p>Ubuntu</p>
+  <div class="col-md-3" id="navigation">
+    <div class="panel-group" id="accordion">
+      <div class="panel panel-default">
+        <div class="panel-heading">
+          <h4 class="panel-title">
+            Virtual Machines
+          </h4>
+        </div>
+        <div id="virtualMachines" class="panel-collapse collapse in">
+          <div class="panel-body">
+            <p>Ubuntu</p>
+          </div>
         </div>
       </div>
-    </div>
-    <div class="panel panel-default">
-      <div class="panel-heading">
-        <h4 class="panel-title">
-          SDKs
-        </h4>
-      </div>
-      <div id="sdks" class="panel-collapse collapse in">
-        <div class="panel-body">
-          <p>OpenJDK</p>
-          <p>Node.js</p>
-          <p>Guard</p>
+      <div class="panel panel-default">
+        <div class="panel-heading">
+          <h4 class="panel-title">
+            Development Kits
+          </h4>
+        </div>
+        <div id="developmentKits" class="panel-collapse collapse in">
+          <div class="panel-body">
+            <p>OpenJDK</p>
+            <p>Node.js</p>
+            <p>Guard</p>
+          </div>
         </div>
       </div>
-    </div>
-    <div class="panel panel-default">
-      <div class="panel-heading">
-        <h4 class="panel-title">
-          Database Servers
-        </h4>
-      </div>
-      <div id="databaseServers" class="panel-collapse collapse in">
-        <div class="panel-body">
+      <div class="panel panel-default">
+        <div class="panel-heading">
+          <h4 class="panel-title">
+            Database Servers
+          </h4>
+        </div>
+        <div id="databaseServers" class="panel-collapse collapse in">
+          <div class="panel-body">
+            <p>PostgreSQL</p>
+            <p>MySQL</p>
+            <p>MariaDB</p>
 
+          </div>
         </div>
       </div>
-    </div>
-    <div class="panel panel-default">
-      <div class="panel-heading">
-        <h4 class="panel-title">
-          Message Brokers
-        </h4>
-      </div>
-      <div id="messageBrokers" class="panel-collapse collapse in">
-        <div class="panel-body">
+      <div class="panel panel-default">
+        <div class="panel-heading">
+          <h4 class="panel-title">
+            Message Brokers
+          </h4>
+        </div>
+        <div id="messageBrokers" class="panel-collapse collapse in">
+          <div class="panel-body">
+            <p>RabbitMQ</p>
+            <p>Eclipse Mosquitto</p>
+          </div>
         </div>
       </div>
-    </div>
-    <div class="panel panel-default">
-      <div class="panel-heading">
-        <h4 class="panel-title">
-          Search Engines
-        </h4>
-      </div>
-      <div id="searchEngines" class="panel-collapse collapse in">
-        <div class="panel-body">
+      <div class="panel panel-default">
+        <div class="panel-heading">
+          <h4 class="panel-title">
+            Search Engines
+          </h4>
+        </div>
+        <div id="searchEngines" class="panel-collapse collapse in">
+          <div class="panel-body">
+            <p>Elasticsearch</p>
+            <p>Solr</p>
+          </div>
         </div>
       </div>
     </div>

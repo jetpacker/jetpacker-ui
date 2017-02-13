7d492ee69b41ccae7a2975f37b5c055c907ce1d7
ruach <slickwolf@gmail.com>
2017-02-13 23:58:55 +0800

    Disable 'no-console' and 'no-alert' temporarily

--- .eslintrc.js
+++ .eslintrc.js
@@ -25,6 +25,8 @@ module.exports = {
       'vue': 'never'
     }],
     // allow debugger during development
-    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
+    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
+    'no-console': 'off',
+    'no-alert': 'off'
   }
 }

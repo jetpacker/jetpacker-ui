f514f95a62b5e0227d727f087d1d824b29453de3
forseti <slickwolf@gmail.com>
2017-02-15 17:50:47 +0800

    Hook Vuex to jetpacker-api

--- src/main.js
+++ src/main.js
@@ -1,18 +1,10 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
-import axios from 'axios';
 
 import App from './App';
 import router from './router';
 import store from './store';
-
-const instance = axios.create({
-  baseURL: 'http://localhost:5000',
-  timeout: 1000,
-});
-
-Vue.prototype.$http = instance;
 
 /* eslint-disable no-new */
 new Vue({

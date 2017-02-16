e292b2bba7b08081a22de7f005f0464b17cb6341
ruach <slickwolf@gmail.com>
2017-02-17 01:25:09 +0800

    Introduce flags.js to handle UI states

--- src/store/modules/presets.js
+++ src/store/modules/presets.js
@@ -1,65 +1,25 @@
-import http from '../http';
-
 const state = {
   presets: {
     machine: {},
-    kits: {
-      packages: {},
-      activePackage: '',
-    },
+    kits: {},
     containers: {
-      activeType: '',
-      dataStores: {
-        packages: {},
-        activePackage: '',
-      },
-      messageBrokers: {
-        packages: {},
-        activePackage: '',
-      },
-      searchEngines: {
-        packages: {},
-        activePackage: '',
-      },
+      dataStores: {},
+      messageBrokers: {},
+      searchEngines: {},
     },
   },
 };
 
 const mutations = {
-  SET_DEFAULTS: (state, defaults) => {
-    state.presets.machine = defaults.machine;
+  SET_PRESETS: (state, data) => {
+    state.presets.machine = data.machine;
+    state.presets.kits = data.kits;
 
-    const kits = state.presets.kits;
-
-    Object.keys(defaults.kits).forEach((key) => {
-      kits.packages[key] = defaults.kits[key];
-    });
-
-    const key = Object.keys(kits.packages)[0];
-    kits.packages[key].active = true;
-    kits.activePackage = key;
-
-    console.log('kits', kits);
-  },
-
-  SET_ACTIVE_PACKAGE_FOR_KITS: (state, activePackage) => {
-    const kits = state.presets.kits;
-    kits.packages[activePackage].active = true;
-    kits.activePackage = activePackage;
+    console.log('kits', state.presets.kits);
   },
 };
 
 const actions = {
-  SET_DEFAULTS: ({ commit }) => {
-    http
-      .get('generator')
-      .then((response) => {
-        commit('SET_DEFAULTS', response.data);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  },
   SET_ACTIVE_PACKAGE_FOR_KITS: ({ commit }, activePackage) => {
     commit('SET_ACTIVE_PACKAGE_FOR_KITS', activePackage);
   },

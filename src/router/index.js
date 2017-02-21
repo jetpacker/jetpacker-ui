808cfaceac75e6d251c93a1aa5d2c6d2630bba3d
forseti <slickwolf@gmail.com>
2017-02-21 12:44:46 +0800

    Link Build and Reset to router under tools

--- src/router/index.js
+++ src/router/index.js
@@ -7,6 +7,8 @@ import Help from '../components/Help';
 import Machine from '../components/settings/Machine';
 import Kit from '../components/settings/Kit';
 import Container from '../components/settings/Container';
+import Build from '../components/tools/Build';
+import Reset from '../components/tools/Reset';
 
 Vue.use(Router);
 
@@ -21,6 +23,9 @@ const router = new Router({
     { path: '/settings/machine', component: Machine },
     { path: '/settings/kits', component: Kit },
     { path: '/settings/containers/:type', component: Container },
+
+    { path: '/tools/build', component: Build },
+    { path: '/tools/reset', component: Reset },
 
     { path: '*', redirect: '/' },
   ],

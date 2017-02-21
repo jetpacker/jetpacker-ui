bd1fb3de9af04575ca18d94bdc21cd79ebed4626
ruach <slickwolf@gmail.com>
2017-02-21 08:24:30 +0800

    Use Vue.set(map, key, value) to make values.js reactive

--- src/store/modules/values.js
+++ src/store/modules/values.js
@@ -1,3 +1,5 @@
+import Vue from 'vue';
+
 const state = {
   machine: {},
   kits: {},
@@ -6,48 +8,49 @@ const state = {
 
 const mutations = {
   SET_VALUES: (state, presets) => {
-    state.machine = {
-      box: Object.keys(presets.machine.box.releases)[0],
-      memory: presets.machine.memory.value,
-      timezone: Intl.DateTimeFormat().resolvedOptions().timeZone,
-    };
+    const machine = state.machine;
+    const kits = state.kits;
+    const containers = state.containers;
+
+    // Use Vue.set will make nested maps reactive
+    Vue.set(machine, 'box', Object.keys(presets.machine.box.releases)[0]);
+    Vue.set(machine, 'memory', presets.machine.memory.value);
+    Vue.set(machine, 'timezone', Intl.DateTimeFormat().resolvedOptions().timeZone);
 
     Object.values(presets.kits).forEach((kit) => {
-      state.kits[kit.name] = {
-        version: kit.version ? kit.version.releases[0] : '',
-        install: kit.install.value ? Boolean(kit.install.value) : false,
-      };
+      Vue.set(kits, kit.name, {});
+      Vue.set(kits[kit.name], 'version', kit.version ? kit.version.releases[0] : '');
+      Vue.set(kits[kit.name], 'install', kit.install.value ? Boolean(kit.install.value) : false);
 
       if (kit.extensions) {
-        state.kits[kit.name].extensions = {};
+        Vue.set(kits[kit.name], 'extensions', {});
+        const extensions = kits[kit.name].extensions;
+
         kit.extensions.forEach((extension) => {
-          state.kits[kit.name].extensions[extension.name] = {
-            install: false,
-            version: extension.version.releases[0],
-          };
+          Vue.set(extensions, extension.name, {});
+          Vue.set(extensions[extension.name], 'install', false);
+          Vue.set(extensions[extension.name], 'version', extension.version.releases[0]);
         });
       }
     });
 
     Object.values(presets.containers).forEach((container) => {
-      state.containers[container.name] = {
-        install: false,
-        version: container.version.releases[0],
-      };
+      Vue.set(containers, container.name, {});
+      Vue.set(containers[container.name], 'install', false);
+      Vue.set(containers[container.name], 'version', container.version.releases[0]);
 
       if (container.parameters) {
-        state.containers[container.name].parameters = {};
+        Vue.set(containers[container.name], 'parameters', {});
+        const parameters = containers[container.name].parameters;
+
         container.parameters.forEach((parameter) => {
-          state.containers[container.name]
-               .parameters[parameter.name] = parameter.value;
+          Vue.set(parameters, parameter.name, parameter.value);
         });
       }
     });
   },
   UPDATE_KIT: (state, payload) => {
-    console.log('payload', payload);
     state.kits[payload.name][payload.attribute] = payload.value;
-    console.log('kits@values', state.kits);
   },
   UPDATE_KIT_EXTENSION: (state, payload) => {
     const kit = state.kits[payload.name];
@@ -55,7 +58,6 @@ const mutations = {
     kit.extensions[extension.name][extension.attribute] = extension.value;
   },
   UPDATE_CONTAINER: (state, payload) => {
-    console.log('payload', payload);
     state.containers[payload.name][payload.attribute] = payload.value;
   },
   UPDATE_CONTAINER_PARAMETER: (state, payload) => {
@@ -73,7 +75,6 @@ const actions = {
     commit('UPDATE_KIT_EXTENSION', payload);
   },
   UPDATE_CONTAINER: ({ commit }, payload) => {
-    console.log('hello');
     commit('UPDATE_CONTAINER', payload);
   },
   UPDATE_CONTAINER_PARAMETER: ({ commit }, payload) => {

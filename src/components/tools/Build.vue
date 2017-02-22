5d166b8fe7e83d1dde5da1b789d38354b95eabbc
ruach <slickwolf@gmail.com>
2017-02-22 14:32:35 +0800

    Bind Virtual Machine summary to Build summary

--- src/components/tools/Build.vue
+++ src/components/tools/Build.vue
@@ -2,7 +2,7 @@
   <div class="panel panel-default">
     <div class="panel-body">
       <div class="col-md-12 jumbotron">
-        <h2 class="build-caption">
+        <h2 class="caption">
           You are almost there!
         </h2>
         <p>
@@ -29,26 +29,81 @@
           Build Jetpack
         </button>
       </div>
-      <div class="col-md-12 build-summary">
-        <h4>
+      <div class="col-md-12 summary">
+        <h4 class="summary-caption">
           <a>
             <i class="fa fa-chevron-circle-down fa-lg"></i>
-            Show Build Summary
+            Hide Your Build Summary
           </a>
         </h4>
 
-        <div class="panel panel-default">
-          <div class="panel-body">
-            <h4>Virtual Machine</h4>
-            <p>...</p>
-            <h4>Development Kits</h4>
-            <p>...</p>
-            <h4>Data Stores</h4>
-            <p>...</p>
-            <h4>Message Brokers</h4>
-            <p>...</p>
-            <h4>Search Engines</h4>
-            <p>...</p>
+        <div class="list-group">
+          <div class="list-group-item">
+            <h4 class="list-group-item-heading">
+              Virtual Machine
+            </h4>
+            <div class="list-group-item-text summary-item-content">
+              <form class="form-horizontal">
+                <p class="form-group">
+                  <span class="col-md-3 form-static-control">
+                    {{ virtualMachine.box.label }}:
+                  </span>
+                  <span class="form-static-control">
+                    {{ virtualMachine.box.value }}
+                  </span>
+                </p>
+                <p class="form-group">
+                  <span class="col-md-3 form-static-control">
+                    {{ virtualMachine.memory.label }}:
+                  </span>
+                  <span class="form-static-control">
+                    {{ virtualMachine.memory.value }}
+                  </span>
+                </p>
+                <p class="form-group">
+                  <span class="col-md-3 form-static-control">
+                    {{ virtualMachine.timezone.label }}:
+                  </span>
+                  <span class="form-static-control">
+                    {{ virtualMachine.timezone.value }}
+                  </span>
+                </p>
+              </form>
+            </div>
+          </div>
+          <div class="list-group-item">
+            <h4 class="list-group-item-heading">
+              Development Kits
+            </h4>
+            <div class="list-group-item-text summary-item-content">
+              aaaaaa
+            </div>
+          </div>
+          <div class="list-group-item">
+            <h4 class="list-group-item-heading">
+              Data Stores
+            </h4>
+            <div class="list-group-item-text">
+              aaaaaa
+            </div>
+          </div>
+
+          <div class="list-group-item">
+            <h4 class="list-group-item-heading">
+              Message Brokers
+            </h4>
+            <p class="list-group-item-text">
+              aaaaaa
+            </p>
+          </div>
+
+          <div class="list-group-item">
+            <h4 class="list-group-item-heading">
+              Search Engines
+            </h4>
+            <p class="list-group-item-text">
+              aaaaaa
+            </p>
           </div>
         </div>
       </div>
@@ -56,16 +111,72 @@
   </div>
 </template>
 
+<script>
+  export default {
+    computed: {
+      values() {
+        return this.$store.getters.values;
+      },
+      virtualMachine() {
+        const presets = this.$store.getters.presets.machine;
+        const values = this.$store.getters.values.machine;
+
+        const virtualMachine = {
+          box: {
+            label: presets.box.label,
+            value: presets.box.releases[values.box],
+          },
+          memory: {
+            label: presets.memory.label,
+            value: values.memory,
+          },
+          timezone: {
+            label: presets.timezone.label,
+            value: values.timezone,
+          },
+        };
+
+        return virtualMachine;
+      },
+//      developmentKits() {
+//        const presets = this.$store.getters.presets.kits;
+//
+//      },
+//      dataStores() {
+//        const containers = this.$store.getters.presets.containers;
+//        const presets = Object.values(containers)
+//          .filter(container => container.type === 'DataStore');
+//      },
+//      messageBrokers() {
+//        const containers = this.$store.getters.presets.containers;
+//        const presets = Object.values(containers)
+//          .filter(container => container.type === 'MessageBroker');
+//      },
+//      searchEngines() {
+//        const containers = this.$store.getters.presets.containers;
+//        const presets Object.values(containers)
+//          .filter(container => container.type === 'SearchEngine');
+//      },
+    },
+  };
+</script>
+
 <style scoped>
   .jumbotron {
     background-color: inherit;
     padding: 0 0 0 0;
     margin-bottom: 5px;
   }
-  .build-caption {
+  .caption {
     margin-top: 0px;
   }
-  .build-summary {
-    padding: 15px 0 5px 0;
+  .summary {
+    padding: 20px 0 5px 0;
+  }
+  .summary-caption {
+    padding-bottom: 20px;
+  }
+  .summary-item-content {
+    padding-top: 10px;
   }
 </style>

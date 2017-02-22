83a5b648785ddd56460e0142765c8ae584894a93
forseti <slickwolf@gmail.com>
2017-02-23 03:22:04 +0800

    Complete Build Summary section

--- src/components/tools/Container.vue
+++ src/components/tools/Container.vue
@@ -9,26 +9,28 @@
       </h4>
     </div>
     <div class="list-group-item-text summary-item-content">
-      {{ summary }}
-      <!--<table class="table table-bordered" v-if="Object.keys(summary).length">-->
-        <!--<thead>-->
-          <!--<tr>-->
-            <!--<th class="col-md-3">Name</th>-->
-            <!--<th>Extensions</th>-->
-          <!--</tr>-->
-        <!--</thead>-->
-        <!--<tbody>-->
-          <!--<tr v-for="(extensions, kit) in summary">-->
-            <!--<td>{{ kit }}</td>-->
-            <!--<td>-->
-              <!--{{ extensions }}-->
-            <!--</td>-->
-          <!--</tr>-->
-        <!--</tbody>-->
-      <!--</table>-->
-      <!--<p v-else>-->
-        <!--None.-->
-      <!--</p>-->
+      <table class="table table-bordered" v-if="Object.keys(summary).length">
+        <thead>
+          <tr>
+            <th class="col-md-3">Name</th>
+            <th>Parameters</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="(parameters, container) in summary">
+            <td>{{ container }}</td>
+            <td v-if="Object.keys(parameters).length">
+              {{ parameters }}
+            </td>
+            <td v-else>
+              None
+            </td>
+          </tr>
+        </tbody>
+      </table>
+      <p v-else>
+        None
+      </p>
     </div>
   </div>
 </template>
@@ -42,6 +44,7 @@
     computed: {
       summary() {
         const summary = {};
+
         const values = this.$store.getters.values.containers;
         const chosenContainers = Object.keys(values)
                                        .filter(key => values[key].install === true);
@@ -49,8 +52,23 @@
         const presets = Object.values(this.$store.getters.presets.containers)
                               .filter(container => container.type === this.name
                                       && chosenContainers.includes(container.name));
+
+        presets.forEach((preset) => {
+          const label = `${preset.label} (${values[preset.name].version})`;
+          const parameters = values[preset.name].parameters;
+
+          summary[label] = {};
+
+          if (parameters) {
+            preset.parameters.forEach((parameter) => {
+              summary[label][parameter.label] = parameters[parameter.name];
+            });
+          }
+        });
+
         console.log(this.name, chosenContainers);
         console.log(this.name, presets);
+        console.log('summary', summary);
 
         return summary;
       },

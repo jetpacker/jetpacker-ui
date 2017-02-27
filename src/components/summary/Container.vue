ea8a6684eecab80a704802e0eb1b442b3ac2de2b
ruach <slickwolf@gmail.com>
2017-02-28 00:40:24 +0800

    Commit the old Container.vue, Kit.vue, and Machine.vue to new summary folder

---
+++ src/components/summary/Container.vue
@@ -0,0 +1,83 @@
+<template>
+  <div class="panel panel-default">
+    <div class="panel-body">
+      <h4 class="summary-item-header">
+        {{ description }}
+        <router-link tag="a" class="pull-right"
+                     :to="'/settings/containers/' + name"
+                     @click.native="setActive(name)">
+          <i class="fa fa-pencil-square-o fa-fw"></i><span class="summary-item-edit">Edit</span>
+        </router-link>
+      </h4>
+      <table class="table table-bordered summary-item-content" v-if="Object.keys(summary).length">
+        <thead>
+          <tr>
+            <th class="col-md-3">Name</th>
+            <th>Parameters</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="(parameters, container) in summary">
+            <td>{{ container }}</td>
+            <td v-if="Object.keys(parameters).length">
+              {{ parameters }}
+            </td>
+            <td v-else>
+              None
+            </td>
+          </tr>
+        </tbody>
+      </table>
+      <p v-else>
+        None
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+  import { mapGetters } from 'vuex';
+  import controls from '../../mixins/controls';
+
+  export default {
+    mixins: [
+      controls,
+    ],
+    props: {
+      name: String,
+      description: String,
+    },
+    computed: {
+      ...mapGetters([
+        'chosenContainers',
+        'presets',
+      ]),
+      summary() {
+        const summary = {};
+        const containers = this.chosenContainers;
+        const presets = Object.values(this.presets.containers)
+                              .filter(container => container.type === this.name
+                                      && container.name in containers);
+
+        presets.forEach((preset) => {
+          const label = `${preset.label} (${containers[preset.name].version})`;
+          const parameters = containers[preset.name].parameters;
+
+          summary[label] = {};
+
+          if (parameters) {
+            preset.parameters.forEach((parameter) => {
+              summary[label][parameter.label] = parameters[parameter.name];
+            });
+          }
+        });
+
+        return summary;
+      },
+    },
+  };
+</script>
+
+<style scoped>
+  @import url("/static/styles/summaryItem.css");
+</style>
---
+++ src/components/summary/Kit.vue
@@ -0,0 +1,93 @@
+<template>
+  <div class="panel panel-default">
+    <div class="panel-body">
+      <h4 class="summary-item-header">
+        Development Kits
+        <router-link tag="a" class="pull-right" to="/settings/kits"
+                     @click.native="setActive(name)">
+          <i class="fa fa-pencil-square-o fa-fw"></i><span class="summary-item-edit">Edit</span>
+        </router-link>
+      </h4>
+      <table class="table table-bordered summary-item-content" v-if="Object.keys(summary).length">
+        <thead>
+          <tr>
+            <th class="col-md-3">Name</th>
+            <th>Extensions</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="(extensions, kit) in summary">
+            <td>{{ kit }}</td>
+            <td v-if="extensions">
+              {{ extensions }}
+            </td>
+            <td v-else>
+              None
+            </td>
+          </tr>
+        </tbody>
+      </table>
+      <p v-else>
+        None
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+  import { mapGetters } from 'vuex';
+  import controls from '../../mixins/controls';
+
+  export default {
+    data() {
+      return {
+        name: 'DevelopmentKit',
+      };
+    },
+    mixins: [
+      controls,
+    ],
+    computed: {
+      ...mapGetters([
+        'chosenKits',
+        'presets',
+      ]),
+      summary() {
+        const summary = {};
+        const kits = this.chosenKits;
+        const presets = this.presets.kits;
+
+        Object.keys(kits).forEach((key) => {
+          const kit = kits[key];
+          const preset = presets[key];
+          let version = 'Latest';
+
+          if (preset.version) {
+            const option = preset.version.options.find(option => option.value === kit.version);
+            version = option.label ? option.label : option.value;
+          }
+
+          const label = `${preset.label} (${version})`;
+          summary[label] = 'None';
+
+          if (kit.extensions) {
+            const convertResultToLabel = function call(result) {
+              return `${result.label} (${kit.extensions[result.name]})`;
+            };
+
+            summary[label] = preset.extensions
+                                   .filter(extension => extension.name in kit.extensions)
+                                   .map(convertResultToLabel)
+                                   .join(', ');
+          }
+        });
+
+        return summary;
+      },
+    },
+  };
+</script>
+
+<style scoped>
+  @import url("/static/styles/summaryItem.css");
+</style>

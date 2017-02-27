429c0b721af188fe5034a6bfece2c98865c8af19
forseti <slickwolf@gmail.com>
2017-02-28 00:39:01 +0800

    Move Summary's buttons to Buttons.vue

---
+++ src/components/summary/Buttons.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="row">
+    <div class="col-sm-6">
+      <button type="button" class="btn btn-default btn-lg btn-block"
+              @click="resetSettings">
+        <i class="fa fa-undo fa-lg"></i>
+        Reset Settings
+      </button>
+    </div>
+    <div class="col-sm-6">
+      <button type="button" class="btn btn-primary btn-lg btn-block"
+              @click="buildJetpack">
+        <i class="fa fa-cloud-download fa-lg"></i>
+        Build Jetpack
+      </button>
+    </div>
+  </div>
+</template>
+
+<script>
+  import { mapActions } from 'vuex';
+  import Summary from './Content';
+
+  export default {
+    components: {
+      appSummary: Summary,
+    },
+    methods: {
+      ...mapActions([
+        'resetSettings',
+        'buildJetpack',
+      ]),
+    },
+  };
+</script>

014c830e88ceb623fcbf2123800e542ea13cd90f
ruach <slickwolf@gmail.com>
2017-02-28 00:37:25 +0800

    Rename the old Summary.vue to Content.vue

---
+++ src/components/summary/Content.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="col-md-12 summary">
+    <app-machine></app-machine>
+    <app-kit></app-kit>
+    <app-container name="DataStore"
+                   description="Data Stores"></app-container>
+    <app-container name="MessageBroker"
+                   description="Message Brokers"></app-container>
+    <app-container name="SearchEngine"
+                   description="Search Engines"></app-container>
+  </div>
+</template>
+
+<script>
+  import Machine from './Machine';
+  import Kit from './Kit';
+  import Container from './Container';
+
+  export default {
+    components: {
+      appMachine: Machine,
+      appKit: Kit,
+      appContainer: Container,
+    },
+  };
+</script>
+
+<style scoped>
+  @import url("/static/styles/summary.css");
+</style>

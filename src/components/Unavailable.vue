9956e68cb6eacf832ccfd01caf005ba6d1110d46
ruach <slickwolf@gmail.com>
2017-02-28 12:01:53 +0800

    Add Unavailable.vue to show Warning when API server is not up

---
+++ src/components/Unavailable.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="main-container">
+    <div class="main-wrapper">
+      <div class="page-info">
+        <h2>
+          <i class="fa fa-warning fa-lg"></i>
+          Ooops, Our Server is Down :(
+        </h2>
+        <h4>
+          We're really sorry that our server is down. Please come back again later.
+          Thanks!!
+        </h4>
+      </div>
+    </div>
+  </div>
+</template>

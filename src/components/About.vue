4b7e484de96a66ac281b1a30d3fe41f63e8a6cc6
forseti <slickwolf@gmail.com>
2018-04-08 05:50:32 +0800

    Added contents to About section

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,13 +1,66 @@
 <template>
   <div class="main-container">
     <div class="row main-wrapper">
-      <div class="page-info full-height">
-        <h1>What is <span class="color-theme">jetpacker.io</span></h1>
+      <div class="page-info">
+        <h1>Anything about <span class="color-theme">jetpackr</span></h1>
         <h4>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-          sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-          Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+          <span class="color-theme">jetpackr</span>
+          generates portable development environment for
+          <i class="fa fa-coffee fa-sm"></i>
+          Java developers
         </h4>
+      </div>
+      <div class="row">
+        <div class="col-sm-offset-1 col-sm-10">
+          <div class="main-wrapper">
+            <div class="panel panel-primary">
+              <div class="panel-body">
+                <h3>What is <span class="color-theme">jetpackr</span></h3>
+                <p>
+                  This is a code generator that creates
+                  a <span class="color-theme">jetpack</span>,
+                  a portable development environment for Java programmers.
+                  <span class="color-theme">jetpacker</span> is inspired
+                  by PuPHPet, Phansible, Rove, and Spring Initializr.
+                </p>
+
+                <h3>What exactly is a <span class="color-theme">jetpack</span></h3>
+                <p>
+                  A <span class="color-theme">jetpack</span> is a development environment
+                  that is powered by Vagrant, Ansible, and Docker. Armed with these tools,
+                  any Java developers can confidently share their projects with anyone without
+                  having to struggle with installation and setup.
+                </p>
+
+                <h3>Do I need to know Vagrant, Ansible, and Docker?</h3>
+                <p>
+                  No. All you need is to follow the instructions in the documentation, generate
+                  a <span class="color-theme">jetpack</span>,
+                  ignite it, and start coding. However, having knowledge in these technologies
+                  will give you more options in customizations. So it's always good to learn them.
+                </p>
+
+                <h3>Who is your target audience?</h3>
+                <p>
+                  Programmers, hobbyists, teachers, students, etc can benefit from this project, regardless
+                  of their experience levels.
+                </p>
+
+                <h3>Can I contribute to this project?</h3>
+                <p>
+                  Of course. We will always welcome any contributions and suggestions.
+                </p>
+
+                <h3>Anything else I need to know?</h3>
+                <p>
+                  This project was also created by us to showcase Spring Boot and Vue.js integration.
+                  Anyone who is learning these stacks, can benefit from
+                  <span class="color-theme">jetpackr</span>'s source codes.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </div>

7ef5d84c8e9533479cbbfd42491f5edf8a2594f2
forseti <slickwolf@gmail.com>
2018-04-08 05:50:10 +0800

    Improved the header texts

--- src/components/Configuration.vue
+++ src/components/Configuration.vue
@@ -4,7 +4,10 @@
       <div class="page-info">
         <h1>Welcome to <span class="color-theme">jetpackr</span></h1>
         <h4>
-          <span class="color-theme">jetpackr</span> generates portable development environment for Java developers.
+          <span class="color-theme">jetpackr</span>
+          generates portable development environment for
+          <i class="fa fa-coffee fa-sm"></i>
+          Java developers
         </h4>
       </div>
       <div class="row">

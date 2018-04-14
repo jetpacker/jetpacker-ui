a54931ebd7e8a397b4eef9ef7007358f8891f353
forseti <slickwolf@gmail.com>
2018-04-14 19:38:54 +0800

    Completed the About part

--- src/components/About.vue
+++ src/components/About.vue
@@ -3,88 +3,60 @@
     <div class="col-sm-offset-2 col-sm-8">
       <div class="main-wrapper">
         <div class="panel panel-primary">
-          <div class="panel-body">
-            <div class="media">
-              <div class="media-body">
-                <h2 class="media-heading">
-                  What is <span class="color-theme">jetpackr</span>
-                </h2>
-                This is a code generator that creates
-                a <span class="color-theme">jetpack</span>,
-                a portable development environment for Java programmers.
-                <span class="color-theme">jetpacker</span> is inspired
-                by PuPHPet, Phansible, Rove, and Spring Initializr.
-              </div>
-            </div>
+          <div class="panel-body info">
+            <h2>About <span class="color-theme">jetpackr</span></h2>
+            <p>
+              <span class="color-theme">jetpackr</span> is a code generator that produces
+              a <span class="color-theme">jetpack</span>, a semi automated isolated
+              development environment that is powered by Vagrant, Ansible,
+              and optionally Docker.
 
-            <div class="media">
-              <div class="media-body">
-                <h2 class="media-heading">
-                  What exactly is a <span class="color-theme">jetpack</span>
-                </h2>
-                A <span class="color-theme">jetpack</span> is a Java
-                development environment that is powered by Vagrant,
-                Ansible, and Docker.
-                Armed with these tools, any Java developers
-                can confidently:
-                <ul>
-                  <li>
-                    Work on several projects that have different application architecture.
-                  </li>
-                  <li>
-                    Share their projects with anyone without having to struggle with initial
-                    installation and setup.
-                  </li>
-                  <li>
-                    Keep their PCs organized and clean from most of the development tools.
-                  </li>
-                </ul>
-              </div>
-            </div>
-
-            <div class="media">
-              <div class="media-body">
-                <h2 class="media-heading">
-                  So I need to know Vagrant, Ansible, and Docker?
-                </h2>
-                Not necessarily. All you need to do is follow the instructions
-                from the documentation, generate
-                a <span class="color-theme">jetpack</span>, ignite it, and start coding.
-                However, having knowledge in these technologies will give
-                you more options in customizations.
-                So it's always good to learn them.
-              </div>
-            </div>
-
-            <div class="media">
-              <div class="media-body">
-                <h2 class="media-heading">
-                  Who is your target audience?
-                </h2>
-                Programmers. This includes freelancers, hobbyists, teachers,
-                students, etc. They can
-                benefit from this project, regardless of their experience levels.
-              </div>
-            </div>
-
-            <div class="media">
-              <div class="media-body">
-                <h2 class="media-heading">
-                  Can I contribute to this project?
-                </h2>
-                Of course. We will always welcome any contributions and suggestions.
-              </div>
-            </div>
-
-            <div class="media">
-              <div class="media-body">
-                <h2 class="media-heading">
-                  Credits
-                </h2>
-                To all developers of the open source libraries and services
-                used by this project.
-              </div>
-            </div>
+              Armed with these tools, any developers can confidently:
+            </p>
+            <ul>
+              <li>
+                Work on several projects that have different application architecture.
+              </li>
+              <li>
+                Share their projects with anyone without having to struggle with
+                common compatibility issues.
+              </li>
+              <li>
+                Skip the manual installations of most applications, services,
+                and tools in their project.
+              </li>
+              <li>
+                Keep their PCs organized and clean from most of the development tools.
+              </li>
+              <li>
+                Experiment with new technologies without worrying of making mistakes.
+              </li>
+            </ul>
+            <p>You don't necessarily need to know Vagrant, Ansible, and Docker to use
+              a <span class="color-theme">jetpack</span>. All you need to do:</p>
+            <ol>
+              <li>
+                Download and install VirtualBox
+              </li>
+              <li>
+                Download and install Vagrant
+              </li>
+              <li>
+                Generate a <span class="color-theme">jetpack</span> from this website.
+              </li>
+              <li>
+                Unzip it as a folder, open a console, and go there.
+              </li>
+              <li>
+                Type: <mark>vagrant up</mark>
+              </li>
+            </ol>
+            That's it!!!
+            <h2>Credits</h2>
+            <p>
+              Special thanks to open source community who work tirelessly days and nights
+              on applications, frameworks, and services used by this project!!!
+            </p>
           </div>
         </div>
       </div>
@@ -101,3 +73,7 @@ export default {
   ],
 };
 </script>
+
+<style scoped>
+  @import url("/static/styles/about.css");
+</style>

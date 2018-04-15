17dc52486a15c78141d9b9626c1c25f8b9e18ccc
forseti <slickwolf@gmail.com>
2018-04-16 00:00:09 +0800

    Simplified footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -4,31 +4,17 @@
           <div class="row">
               <div class="col-md-12">
                   <span class="copyright">
-                    From
-                    <a class="creator" :href="wastefactory_url">wastefactory</a>
-                    and
-                    <a class="creator" :href="forseti_url">forseti</a>
-                    with
+                    Made with
                     <i class="fa fa-heart fa-sm heart"></i>
                     <i class="fa fa-heart fa-sm heart"></i>
                     <i class="fa fa-heart fa-sm heart"></i>
+                    by geeks for geeks
                   </span>
               </div>
           </div>
       </div>
   </footer>
 </template>
-
-<script>
-export default {
-  data() {
-    return {
-      wastefactory_url: false,
-      forseti_url: false,
-    };
-  },
-};
-</script>
 
 <style scoped>
   @import url("/static/styles/footer.css");

ea833b70290154bd48af8b9be345bb3e2a1e9ee6
forseti <slickwolf@gmail.com>
2018-04-20 01:38:00 +0800

    Fixed the 'index' issue

--- src/components/guide/Navigation.vue
+++ src/components/guide/Navigation.vue
@@ -3,13 +3,13 @@
     <div class="side-nav">
         <ul class="nav nav-pills nav-stacked nav-container"
             v-for="(menu, index) in menus.guide"
-            :key="index">
+            :key="`menu-${index}`">
           <router-link v-for="(value, key) in menu"
                        :key="key"
                        :to="value.url" tag="li"
                        @click.native="setActive(key)"
                        :class="{ 'active': value.active }">
-            <a class="nav-label" v-bind:class="{ 'builder-btn': index == 0 }">
+            <a class="nav-label builder-btn">
               <span class="fa-stack fa-fw">
                 <i :class="[ 'fa', value.icon ]"></i>
               </span>
@@ -18,7 +18,6 @@
             </a>
           </router-link>
         </ul>
-        <p v-if="index < (menus.guide.length - 1)"></p>
     </div>
   </div>
 </template>
